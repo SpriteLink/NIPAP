9a39975f3f44a75cc9cdc4c00cc64951d6eb4a77
Kristian Larsson <kristian@spritelink.net>
2011-07-29 13:55:06 +0200

    Fiddled some more with octave
    I accidentally put some operators out of order which resulted in title
    and x/ylabels being omitted. Now everything is back to order. Also
    changed name of the outputted pictures.
    
    Dunno about the size though, something smaller than 640x480 would be
    nice but octave seems to omit labels if it's smaller.

--- tests/performance/01-insert/insert.m
+++ tests/performance/01-insert/insert.m
@@ -1,14 +1,14 @@
 
 % 
-title('Prefix insertion time');
-xlabel('Prefix number');
-ylabel('Insert time [s]');
 
 % plot first graph - insertion time before optimization
 d0 = load('data.insert-start');
 plot(d0(:,1), d0(:,2), 'r');
 legend('baseline');
-print('plot-before.png','-dpng','-S500,350');
+title('Prefix insertion time');
+xlabel('Prefix number');
+ylabel('Insert time [s]');
+print('plot-before.png','-dpng','-S640,480');
 
 hold('on');
 % insertion time after optimization, retaining the old plot (ie before
@@ -16,6 +16,8 @@ hold('on');
 d1 = load('data.insert-after-ip4r');
 plot(d1(:,1), d1(:,2), 'b');
 legend('before ip4r', 'after ip4r');
+xlabel('Prefix number');
+ylabel('Insert time [s]');
 print('plot-before-and-after.png','-dpng','-S640,480');
 
 % plot our long running test in a new graph
@@ -24,5 +26,7 @@ d2 = load('data.insert-4610-24s');
 plot(d2(:,1), d2(:,2), 'r');
 legend('after ip4r');
 title('Prefix insertion time');
+xlabel('Prefix number');
+ylabel('Insert time [s]');
 print('plot-after-4610-24s.png','-dpng','-S640,480');
 
