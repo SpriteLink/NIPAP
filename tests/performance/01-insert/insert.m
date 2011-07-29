52142d283e8bfa907828731c756882fc1fb3ed03
Kristian Larsson <kristian@spritelink.net>
2011-07-29 12:54:23 +0200

    Merged octave scripts

--- tests/performance/01-insert/insert.m
+++ tests/performance/01-insert/insert.m
@@ -1,11 +1,28 @@
-d0 = load('data.insert-start');
-d1 = load('data.insert-after-ip4r');
-plot(d0(:,1), d0(:,2), 'r');
-hold('on');
-plot(d1(:,1), d1(:,2), 'b');
+
+% 
+title('Prefix insertion time');
 xlabel('Prefix number');
 ylabel('Insert time [s]');
+
+% plot first graph - insertion time before optimization
+d0 = load('data.insert-start');
+plot(d0(:,1), d0(:,2), 'r');
+legend('baseline');
+print('plot-before.png','-dpng','-S500,350');
+
+hold('on');
+% insertion time after optimization, retaining the old plot (ie before
+% optimization) and printing a combined graph
+d1 = load('data.insert-after-ip4r');
+plot(d1(:,1), d1(:,2), 'b');
 legend('before ip4r', 'after ip4r');
+print('plot-before-and-after.png','-dpng','-S640,480');
+
+% plot our long running test in a new graph
+hold('off');
+d2 = load('data.insert-4610-24s');
+plot(d2(:,1), d2(:,2), 'r');
+legend('after ip4r');
 title('Prefix insertion time');
-print('plot.png','-dpng','-S640,480');
+print('plot-after-4610-24s.png','-dpng','-S640,480');
 
