c3f2161b740b55af604ffe92748abb4f871d72f4
Kristian Larsson <kristian@spritelink.net>
2011-08-09 11:27:47 +0200

    Use xml-test for ip4r testing
    This does a search using the >> operator, which means it will run slow
    without ip4r indices.
    
    Part of issue #7

--- napd/xml-test.py
+++ napd/xml-test.py
@@ -9,34 +9,35 @@ parser.add_option('-p', '--port', dest='port', type='int', default='1337', help=
 
 (options, args) = parser.parse_args()
 
-server_url = 'http://tone.tele2.net:%(port)d/XMLRPC' % { 'port': options.port }
-print server_url
+server_url = 'http://127.0.0.1:%(port)d/XMLRPC' % { 'port': options.port }
 server = xmlrpclib.Server(server_url, allow_none=1);
+
+print server.search_prefix({ 'name': 'global' }, {'operator': 'contained_within', 'val2': '130.244.0.0/30', 'val1': 'prefix'}, {})
 
 #
 # echo test
 #
-print "try the echo function without args"
-args = {}
-print "ARGS:", args
-print "RESULT:", server.echo()
-print ""
-
-print "try the echo function with a message argument"
-args = { 'message': 'Please reply to me, Obi-Wan Kenobi, you are my only hope!' }
-print "ARGS:", args
-print "RESULT:", server.echo( args )
-print ""
+#print "try the echo function without args"
+#args = {}
+#print "ARGS:", args
+#print "RESULT:", server.echo()
+#print ""
+#
+#print "try the echo function with a message argument"
+#args = { 'message': 'Please reply to me, Obi-Wan Kenobi, you are my only hope!' }
+#print "ARGS:", args
+#print "RESULT:", server.echo( args )
+#print ""
 
 
 #
 # try list function
 #
-print "try the list prefix function with a node argument"
-args = { 'node': 'kst5-core-3' }
-print "ARGS:", args
-print "RESULT:", server.list_prefix( args )
-print ""
+#print "try the list prefix function with a node argument"
+#args = { 'node': 'kst5-core-3' }
+#print "ARGS:", args
+#print "RESULT:", server.list_prefix( args )
+#print ""
 
 
 
