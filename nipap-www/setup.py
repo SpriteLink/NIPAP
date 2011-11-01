c6d0234c5d0d60f7a7f687c4a7ebac2979394c08
Lukas Garberg <lukas@spritelink.net>
2011-11-01 09:04:12 +0100

    Prepared nipap-www for packaging
    Added debian-files and moved files around a bit. Removed the
    ez_setup.py-script as it is not used. Modified config file and altered
    setup.py to place the config file in /etc/nipap.

--- nipap-www/setup.py
+++ nipap-www/setup.py
@@ -1,31 +1,31 @@
-try:
-    from setuptools import setup, find_packages
-except ImportError:
-    from ez_setup import use_setuptools
-    use_setuptools()
-    from setuptools import setup, find_packages
+from setuptools import setup, find_packages
 
 setup(
     name='nipap-www',
-    version='0.1',
-    description='',
-    author='',
-    author_email='',
-    url='',
+    version='0.5.0',
+    description='web frontend for NIPAP',
+    author='Kristian Larsson, Lukas Garberg',
+    author_email='kll@tele2.net, lukas@spritelink.net',
+    url='https://github.com/plajjan/NIPAP',
     install_requires=[
         "Pylons>=1.0",
         "Jinja2",
+        "pynipap",
+        "nipapd"
     ],
-    setup_requires=["PasteScript>=1.6.3"],
+#    setup_requires=["PasteScript>=1.6.3"],
     packages=find_packages(exclude=['ez_setup']),
     include_package_data=True,
     test_suite='nose.collector',
     package_data={'nipapwww': ['i18n/*/LC_MESSAGES/*.mo']},
+    data_files = [
+        ( '/etc/nipap/', [ 'nipap-www.ini' ] ),
+    ],
     #message_extractors={'nipapwww': [
     #        ('**.py', 'python', None),
     #        ('public/**', 'ignore', None)]},
     zip_safe=False,
-    paster_plugins=['PasteScript', 'Pylons'],
+#    paster_plugins=['PasteScript', 'Pylons'],
     entry_points="""
     [paste.app_factory]
     main = nipapwww.config.middleware:make_app
