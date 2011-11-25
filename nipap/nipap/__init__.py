45a7d5b27ab8f0f2a00fb44b7c640b7740c524a1
Lukas Garberg <lukas@spritelink.net>
2011-11-25 17:00:02 +0100

    Bumped version

--- nipap/nipap/__init__.py
+++ nipap/nipap/__init__.py
@@ -1,4 +1,4 @@
-__version__		= "0.5.6"
+__version__		= "0.5.7"
 __author__		= "Kristian Larsson, Lukas Garberg"
 __author_email__ = "kll@tele2.net, lukas@spritelink.net"
 __copyright__	= "Copyright 2011, Kristian Larsson, Lukas Garberg"
