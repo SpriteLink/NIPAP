084b9ea6ad8de3b885588c876b10e7238d24436f
Lukas Garberg <lukas@spritelink.net>
2012-01-16 14:52:01 +0100

    Added warning that object might be altered

--- jnipap/jnipap/AddPrefixOptions.java
+++ jnipap/jnipap/AddPrefixOptions.java
@@ -4,5 +4,8 @@ import java.util.HashMap;
 
 /**
  * A class which contains options to the API function add_prefix.
+ *
+ * NOTE: The AddPrefixOptions hash is changed by some API calls, so please do
+ * not reuse!
  */
 public class AddPrefixOptions extends HashMap { }
