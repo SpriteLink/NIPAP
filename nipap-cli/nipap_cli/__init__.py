0738264d8a18f86c7e7b043a406c0e820b83b95c
Lukas Garberg <lukas@spritelink.net>
2012-07-26 12:55:51 +0200

    Released version 0.8.0

--- nipap-cli/nipap_cli/__init__.py
+++ nipap-cli/nipap_cli/__init__.py
@@ -1,4 +1,4 @@
-__version__     = "0.7.0"
+__version__     = "0.8.0"
 __author__      = "Kristian Larsson, Lukas Garberg"
 __author_email__ = "kll@tele2.net, lukas@spritelink.net"
 __license__     = "MIT"
