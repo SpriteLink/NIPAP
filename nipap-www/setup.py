cfe837f11fd8fd40bb61589cce2e995ae6556f1f
Lukas Garberg <lukas@spritelink.net>
2013-02-12 17:31:52 +0100

    Also bumped www-ui version to 0.15.0...

--- nipap-www/setup.py
+++ nipap-www/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='nipap-www',
-    version='0.14.0',
+    version='0.15.0',
     description='web frontend for NIPAP',
     author='Kristian Larsson, Lukas Garberg',
     author_email='kll@tele2.net, lukas@spritelink.net',
