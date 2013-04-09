b83cf0b48dd1d4e580e10534a9b84934b4fad511
Lukas Garberg <lukas@spritelink.net>
2013-04-09 22:31:42 +0200

    Bumped version numbers to 0.16.1

--- nipap-cli/nipap_cli/__init__.py
+++ nipap-cli/nipap_cli/__init__.py
@@ -1,4 +1,4 @@
-__version__     = "0.16.0"
+__version__     = "0.16.1"
 __author__      = "Kristian Larsson, Lukas Garberg"
 __author_email__ = "kll@tele2.net, lukas@spritelink.net"
 __license__     = "MIT"
