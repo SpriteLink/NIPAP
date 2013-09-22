ca60b08f22a773cee4af8cacab385407bbecf276
Kristian Larsson <kristian@spritelink.net>
2013-09-22 13:30:52 +0200

    Update changelogs for v0.22.0

--- nipap-www/nipapwww/__init__.py
+++ nipap-www/nipapwww/__init__.py
@@ -1,4 +1,4 @@
-__version__     = "0.21.0"
+__version__     = "0.22.0"
 __author__      = "Kristian Larsson, Lukas Garberg"
 __author_email__ = "kll@tele2.net, lukas@spritelink.net"
 __license__     = "MIT"
