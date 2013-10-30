0177d2d24b33a117d4ae486d40853b6a8f35bb7e
Lukas Garberg <lukas@spritelink.net>
2013-10-30 15:09:30 +0100

    Bumped version to 0.22.4

--- pynipap/pynipap.py
+++ pynipap/pynipap.py
@@ -147,7 +147,7 @@
 import xmlrpclib
 import logging
 
-__version__		= "0.22.3"
+__version__		= "0.22.4"
 __author__		= "Kristian Larsson, Lukas Garberg"
 __author_email__= "kll@tele2.net, lukas@spritelink.net"
 __copyright__	= "Copyright 2011, Kristian Larsson, Lukas Garberg"
