b8c7516997f31b8bf08ac1483143cbbfb8bd7543
Kristian Larsson <kristian@spritelink.net>
2013-12-30 12:24:26 +0100

    Add/remove remove-icon on click in VRF selector
    Fixes #380.

--- nipap-www/nipapwww/public/nipap.js
+++ nipap-www/nipapwww/public/nipap.js
@@ -1028,6 +1028,7 @@ function clickFilterVRFSelector(evt) {
 
 		// show tick mark
 		$('#vrf_filter_entry_' + String(vrf.id)).children().children('.selector_tick').html('&#10003;');
+		$('#vrf_filter_entry_' + String(vrf.id)).children('.selector_x').html('<img src="/remove-icon.png">');
 
 	} else {
 
@@ -1037,6 +1038,7 @@ function clickFilterVRFSelector(evt) {
 
 		// remove tick mark
 		$('#vrf_filter_entry_' + String(vrf.id)).children().children('.selector_tick').html('&nbsp;');
+		$('#vrf_filter_entry_' + String(vrf.id)).children('.selector_x').html('');
 
 	}
 
