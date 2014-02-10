d6d0a7985373f3e071f132845c2b3b484ca69c1b
Kristian Larsson <kristian@spritelink.net>
2014-02-10 15:51:05 +0100

    Bump version to v0.24.1

--- nipap/nipap/__init__.py
+++ nipap/nipap/__init__.py
@@ -1,4 +1,4 @@
-__version__		= "0.24.0"
+__version__		= "0.24.1"
 __author__		= "Kristian Larsson, Lukas Garberg"
 __author_email__ = "kll@tele2.net, lukas@spritelink.net"
 __copyright__	= "Copyright 2011-2014, Kristian Larsson, Lukas Garberg"
