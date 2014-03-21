411c9288faf57b7658c886db5efe65d03cb832dd
Kristian Larsson <kristian@spritelink.net>
2014-03-21 23:41:33 +0100

    Release v0.25.0 - Dr. Who

--- nipap-www/nipapwww/__init__.py
+++ nipap-www/nipapwww/__init__.py
@@ -1,4 +1,4 @@
-__version__     = "0.24.5"
+__version__     = "0.25.0"
 __author__      = "Kristian Larsson, Lukas Garberg"
 __author_email__ = "kll@tele2.net, lukas@spritelink.net"
 __license__     = "MIT"
