31633fa49a710fa385a2091c8075971a351867e8
Lukas Garberg <lukas@spritelink.net>
2014-05-05 22:21:22 +0200

    Angularified the pool list page
    The pool list page is now rendered with AngularJS.

--- nipap-www/nipapwww/public/controllers.js
+++ nipap-www/nipapwww/public/controllers.js
@@ -62,3 +62,60 @@ nipapApp.controller('VRFListController', function ($scope, $http) {
 		});
 	}
 });
+
+
+/*
+ * PoolListController - used to list pools on the /pool/list-page
+ */
+nipapApp.controller('PoolListController', function ($scope, $http) {
+
+	// Fetch VRFs from backend
+	$http.get('/xhr/list_pool').success(function (data) {
+		if (data.hasOwnProperty('error')) {
+			// display some error message
+		} else {
+			$scope.pools = data;
+		}
+	});
+
+
+	/*
+	 * Callback function after a pool has been removed
+	 */
+	$scope.poolRemoved = function (data, pool) {
+
+		if ('error' in data) {
+			showDialogNotice('Error', data.message);
+			return;
+		}
+
+		var index = $scope.pools.indexOf(pool);
+		$scope.pools.splice(index, 1);
+
+	}
+
+
+	/*
+	 * Display remove confirmation dialog
+	 */
+	$scope.poolConfirmRemove = function (evt, pool) {
+		evt.preventDefault();
+		var dialog = showDialogYesNo('Really remove pool?', 'Are you sure you want to remove the pool "' + pool.name + '"?',
+		function () {
+			var data = {
+				'id': pool.id
+			};
+			$http.get('/xhr/remove_pool', { 'params': data })
+				.success(function (data) {
+					$scope.poolRemoved(data, pool);
+				})
+				.error(function (data, stat) {
+					var msg = data || "Unknown failure";
+					showDialogNotice('Error', stat + ': ' + msg);
+				});
+
+			dialog.dialog('close');
+
+		});
+	}
+});
