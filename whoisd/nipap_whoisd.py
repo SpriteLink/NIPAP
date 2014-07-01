0d376d6fe5f0d80b53bab53a0d4577725312a8f9
Kristian Larsson <kristian@spritelink.net>
2014-07-01 10:30:56 +0200

    Bump to version 0.25.1
    Signing of Debian packages!
    
    Part of #162.

--- whoisd/nipap_whoisd.py
+++ whoisd/nipap_whoisd.py
@@ -1,6 +1,6 @@
 import os
 
-__version__		= "0.24.4"
+__version__		= "0.25.1"
 __author__		= "Kristian Larsson, Lukas Garberg"
 __author_email__ = "kll@tele2.net, lukas@spritelink.net"
 __copyright__	= "Copyright 2011-2014, Kristian Larsson, Lukas Garberg"
