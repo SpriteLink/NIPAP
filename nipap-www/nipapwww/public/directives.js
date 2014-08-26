92ccee1481f68587745742708a61746d01294952
Lukas Garberg <lukas@spritelink.net>
2014-08-26 22:07:25 +0200

    Added a directive for choosing a pool
    Created an AngularJS directive nipapPoolSelector used for choosing
    pools.

--- nipap-www/nipapwww/public/directives.js
+++ nipap-www/nipapwww/public/directives.js
@@ -3,3 +3,22 @@
  */
 
 var nipapAppDirectives = angular.module('nipapApp.directives', []);
+
+nipapAppDirectives.directive('nipapPoolSelector', function ($http) {
+
+    return {
+        restrict: 'AE',
+        templateUrl: '/templates/pool_selector.html',
+        link: function (scope, elem, attrs) {
+            // Fetch Pools from backend
+            $http.get('/xhr/list_pool').success(function (data) {
+                if (data.hasOwnProperty('error')) {
+                    // TODO: display some error message
+                } else {
+                    scope.pools = data;
+                }
+            });
+        }
+    };
+
+});
