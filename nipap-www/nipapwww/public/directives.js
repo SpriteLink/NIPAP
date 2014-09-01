395e987636cbb70cf70db96bf1b95907fc928536
Lukas Garberg <lukas@spritelink.net>
2014-09-01 18:40:09 +0200

    Work on directive for selecting VRF
    Began working on a directive for a VRF selector component used for
    selecting VRFs, first and foremost to be used on the add prefix-page.
    Still some issues to be ironed out...

--- nipap-www/nipapwww/public/directives.js
+++ nipap-www/nipapwww/public/directives.js
@@ -23,11 +23,92 @@ nipapAppDirectives.directive('nipapPoolSelector', function ($http) {
 
 });
 
-nipapAppDirectives.directive('nipapVRFSelector', function ($http) {
+nipapAppDirectives.directive('nipapVrfSelector', function ($http, $timeout) {
 
     return {
         restrict: 'AE',
-        templateUrl: ''
+        templateUrl: '/templates/vrf_selector.html',
+        scope: {
+            selected_vrf: '=selectedVrf'
+        },
+        link: function (scope, elem, attr) {
+
+            scope.query_string = '';
+            scope.timeout_promise = null;
+            scope.search_result = [];
+            scope.selected_vrf = null;
+            scope.query_id = 0;
+            scope.internal_seleted_vrf = scope.selected_vrf || { 'id': null };
+
+            /*
+             * Function ran when VRF search query string has changed
+             */
+            scope.vrfQueryStringChanged = function () {
+                // cancel earlier timeout if it exists
+                if (scope.timeout_promise !== null) {
+                    $timeout.cancel(scope.timeout_promise);
+                }
+                scope.timeout_promise = $timeout(scope.performVRFSearch, 200);
+            }
+
+            /*
+             * Perform VRF search
+             */
+            scope.performVRFSearch = function () {
+
+                var search_q = {
+                    'query_id': scope.query_id,
+                    'query_string': $.trim(scope.query_string),
+                    'max_result': 10,
+                    'offset': 0
+                };
+
+                // If search string empty, search for default VRF
+                if (search_q['query_string'] == '') {
+                    search_q['vrf_id'] = 0;
+                }
+
+                $http.get('/xhr/smart_search_vrf', { 'params': search_q })
+                    .success(scope.receiveVRFSearchResult)
+                    .error(function (data, stat) {
+                        var msg = data || "Unknown failure";
+                        showDialogNotice('Error', stat + ': ' + msg);
+                    });
+
+                scope.query_id += 1;
+
+            }
+
+            /*
+             * Search result returned
+             */
+            scope.receiveVRFSearchResult = function (data) {
+                if (data.hasOwnProperty('error')) {
+                    showDialogNotice('Error', data.message);
+                    return;
+                }
+
+                // Verify that received response is for latest query
+                if (parseInt(data.search_options.query_id) != scope.query_id - 1) {
+                    return;
+                }
+
+                scope.search_result = data.result;
+
+            }
+
+            /*
+             * A VRF is selected
+             */
+            scope.selectVRF = function (vrf) {
+
+                // set VRF
+                scope.internal_selected_vrf = vrf;
+
+                // TODO: close selector box
+
+            }
+        }
     };
 
 });
