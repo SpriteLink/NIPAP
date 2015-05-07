f0f931a47cdb7e17cbff2477d92935b8bfe86af3
Kristian Larsson <kristian@spritelink.net>
2015-05-07 23:35:33 +0200

    Bump version to v0.28.1

--- nipap-cli/nipap_cli/__init__.py
+++ nipap-cli/nipap_cli/__init__.py
@@ -1,4 +1,4 @@
-__version__     = "0.28.0"
+__version__     = "0.28.1"
 __author__      = "Kristian Larsson, Lukas Garberg"
 __author_email__ = "kll@tele2.net, lukas@spritelink.net"
 __license__     = "MIT"
