d6becc6aa2c70038ddfa8a8d80ae96f692343246
Kristian Larsson <kristian@spritelink.net>
2015-05-27 22:01:57 +0200

    Bump version to 0.28.3

--- nipap-cli/nipap_cli/__init__.py
+++ nipap-cli/nipap_cli/__init__.py
@@ -1,4 +1,4 @@
-__version__     = "0.28.2"
+__version__     = "0.28.3"
 __author__      = "Kristian Larsson, Lukas Garberg"
 __author_email__ = "kll@tele2.net, lukas@spritelink.net"
 __license__     = "MIT"
--- whoisd/nipap_whoisd.py
+++ whoisd/nipap_whoisd.py
@@ -1,6 +1,6 @@
 import os
 
-__version__		= "0.28.2"
+__version__		= "0.28.3"
 __author__		= "Kristian Larsson, Lukas Garberg"
 __author_email__ = "kll@tele2.net, lukas@spritelink.net"
 __copyright__	= "Copyright 2011-2014, Kristian Larsson, Lukas Garberg"
