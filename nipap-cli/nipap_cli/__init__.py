0f4d22f7726af9b31765186db3e55f062157df86
Kristian Larsson <kristian@spritelink.net>
2015-07-16 13:07:11 +0200

    Update NEWS file and bump version to v0.28.4

--- nipap-cli/nipap_cli/__init__.py
+++ nipap-cli/nipap_cli/__init__.py
@@ -1,4 +1,4 @@
-__version__     = "0.28.3"
+__version__     = "0.28.4"
 __author__      = "Kristian Larsson, Lukas Garberg"
 __author_email__ = "kll@tele2.net, lukas@spritelink.net"
 __license__     = "MIT"
--- nipap/nipap/__init__.py
+++ nipap/nipap/__init__.py
@@ -1,4 +1,4 @@
-__version__		= "0.28.3"
+__version__		= "0.28.4"
 __db_version__	= 6
 __author__		= "Kristian Larsson, Lukas Garberg"
 __author_email__ = "kll@tele2.net, lukas@spritelink.net"
