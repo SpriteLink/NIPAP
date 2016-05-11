6d5c6f42d1655cdef070d3c0f16a64d43b08e56e
Lukas Garberg <lukas@spritelink.net>
2016-05-11 07:28:36 +0200

    jnipap: Handle new query intepretation format
    Since commits
    
    * 6250100704d6d432dffd8270953a3bf7b53369c9
    * 23563a0d181094b188b1348033f8bc509e79afa7
    * 0af69f7b9cc697ad03a65fe07af14d975dd8cdd1
    
    the format of the query interpretation returned by the smart_search functions
    has changed. This commit updates the jnipap client library to handle the new
    format.
    
    Fixes #914

--- jnipap/jnipap/VRF.java
+++ jnipap/jnipap/VRF.java
@@ -176,13 +176,7 @@ public class VRF extends Jnipap {
 		// extract data from result
 		HashMap ret = new HashMap();
 		ret.put("search_options", (Map)result.get("search_options"));
-
-		Object[] interpretation_result = (Object[])result.get("interpretation");
-		List ret_interpretation = new ArrayList();
-		for (int i = 0; i < interpretation_result.length; i++) {
-			ret_interpretation.add((Map)interpretation_result[i]);
-		}
-		ret.put("interpretation", ret_interpretation);
+		ret.put("interpretation", (Map)result.get("interpretation"));
 
 		ArrayList ret_vrfs = new ArrayList();
 		Object[] result_vrfs = (Object[])result.get("result");
