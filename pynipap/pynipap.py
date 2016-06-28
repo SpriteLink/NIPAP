3fa36c8c41d54151431bdb41bddaa8c15165eaeb
Kristian Larsson <kristian@spritelink.net>
2016-06-28 17:45:05 +0200

    Update NEWS and bump to v0.29.0

--- pynipap/pynipap.py
+++ pynipap/pynipap.py
@@ -160,7 +160,7 @@ if sys.version_info[0] < 3:
 else:
     import xmlrpc.client as xmlrpclib
 
-__version__		= "0.28.4"
+__version__		= "0.29.0"
 __author__		= "Kristian Larsson, Lukas Garberg"
 __author_email__= "kll@tele2.net, lukas@spritelink.net"
 __copyright__	= "Copyright 2011, Kristian Larsson, Lukas Garberg"
