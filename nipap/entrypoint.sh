09a4bcd45296c7618634e4ea0d12761ee727cedb
Kristian Larsson <kristian@spritelink.net>
2016-08-04 14:40:16 +0200

    Dockerify nipapd
    This adds a Dockerfile for nipapd and an entrypoint script which will
    write the config file and launch nipapd.

---
+++ nipap/entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+envtpl /nipap/nipap.conf.dist -o /etc/nipap/nipap.conf
+
+/usr/sbin/nipap-passwd create-database
+if [ -n "$NIPAP_USERNAME" -a -n "$NIPAP_PASSWORD" ]; then
+	echo "Creating user '$NIPAP_USERNAME'"
+	/usr/sbin/nipap-passwd add --username $NIPAP_USERNAME --name "NIPAP user" --password $NIPAP_PASSWORD
+fi
+
+exec /usr/sbin/nipapd --debug --foreground --auto-install-db --auto-upgrade-db --no-pid-file
