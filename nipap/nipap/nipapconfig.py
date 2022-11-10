8b49a146382930234b38f559b2e382d4cbfc4da6
Lukas Garberg <lukas.garberg@tele2.com>
2022-11-10 08:08:26 +0100

    nipapd: Moved config defaults
    Moved config defaults from nipapd to nipapconfig module. Also updated
    handling of missing values in the config file, as None is no longer
    permitted by the configparser module.

--- nipap/nipap/nipapconfig.py
+++ nipap/nipap/nipapconfig.py
@@ -1,5 +1,28 @@
 import configparser
 
+DEFAULT = {
+    'syslog': 'false',
+    'debug': 'false',
+    'foreground': 'false',
+    'forks': 0,
+    'pid_file': '',
+    'listen': '127.0.0.1',
+    'port': '1337',
+    'ssl_port': '',
+    'ssl_cert_file': '',
+    'ssl_key_file': '',
+    'db_host': 'localhost',
+    'db_name': 'nipap',
+    'db_port': '',
+    'db_user': 'nipap',
+    'db_pass': 'papin',
+    'db_sslmode': 'require',
+    'auth_cache_timeout': '3600',
+    'user': '',
+    'group': ''
+}
+
+
 
 class NipapConfig(configparser.ConfigParser):
     """ Makes configuration data available.
@@ -13,20 +36,17 @@ class NipapConfig(configparser.ConfigParser):
     _config = None
     _cfg_path = None
 
-    def __init__(self, cfg_path=None, default=None):
+    def __init__(self, cfg_path=None):
         """ Takes config file path and command line arguments.
         """
 
         self.__dict__ = self.__shared_state
 
-        if default is None:
-            default = {}
-
         if len(self.__shared_state) == 0:
             # First time - create new instance!
             self._cfg_path = cfg_path
 
-            configparser.ConfigParser.__init__(self, default, inline_comment_prefixes=";#")
+            configparser.ConfigParser.__init__(self, DEFAULT, inline_comment_prefixes=";#")
 
             self.read_file()
 
