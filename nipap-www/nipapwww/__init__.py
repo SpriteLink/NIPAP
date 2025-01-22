8a9c7de35f0253ec41cd50a39e1900aaae9049ff
Lukas Garberg <lukas@spritelink.net>
2025-01-22 13:48:36 +0100

    Bumped version vo v0.32.7

--- nipap-www/nipapwww/__init__.py
+++ nipap-www/nipapwww/__init__.py
@@ -1,4 +1,4 @@
-__version__     = "0.32.6"
+__version__     = "0.32.7"
 __author__      = "Kristian Larsson, Lukas Garberg"
 __author_email__ = "kll@tele2.net, lukas@spritelink.net"
 __license__     = "MIT"
